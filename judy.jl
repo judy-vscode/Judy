b5e3ea4fa71addd17cbc4c5ab500c3339d3b49ac
zhiqi lin <ralzq01@gmail.com>
2018-12-05 22:55:22 +0800

    delete debugger vars from collected info

--- judy.jl
+++ judy.jl
@@ -1,82 +1,91 @@
+
+module Connecter
+
 using Sockets
 import JSON
 
 include("EventHandler.jl")
 include("MsgHandler.jl")
 
-server = listen(8000)
-sock = accept(server)
-client = connect(18001)
+function run()
+  server = listen(8000)
+  sock = accept(server)
+  client = connect(18001)
 
-while isopen(sock)
-  # get events
-  msg = MsgHandler.msgRecv(sock)
-  result = Dict()
-  event = ""
-  event_method = ""
+  while isopen(sock)
+    # get events
+    msg = MsgHandler.msgRecv(sock)
+    result = Dict()
+    event = ""
+    event_method = ""
 
-  # handle events
-  id, method, params = MsgHandler.msgParse(msg)
-  println(method)
+    # handle events
+    id, method, params = MsgHandler.msgParse(msg)
+    println(method)
 
-  if method == "initialize"
-    EventHandler.readSourceToAST(ARGS[1])
-    event = Dict()
-    event_method = "initialized"
+    if method == "initialize"
+      EventHandler.readSourceToAST(ARGS[1])
+      event = Dict()
+      event_method = "initialized"
 
-  elseif method == "launch"
-    if !haskey(params, "stopOnEntry")
-      EventHandler.run()
+    elseif method == "launch"
+      if !haskey(params, "stopOnEntry")
+        EventHandler.run()
+        event, event_method = EventHandler.getStatus("breakpoint")
+      else
+        event_method = "stopped"
+        event = Dict("reason" => "entry",
+                     "description" => "stop on entry",
+                     "text" => " ")
+      end
+
+    elseif method == "setBreakPoints"
+      filePath = params["path"]
+      lineno = params["lines"]
+      result = EventHandler.setBreakPoints(filePath, lineno)
+
+    elseif method == "configurationDone"
+      result = Dict()
+
+    elseif method == "continue"
+      result = EventHandler.continous()
       event, event_method = EventHandler.getStatus("breakpoint")
+
+    elseif method == "next"
+      EventHandler.stepOver()
+      event, event_method = EventHandler.getStatus("step")
+
+    elseif method == "stackTrace"
+      result = EventHandler.getStackTrace()
+
+    elseif method == "scopes"
+      frame_id = params["frameId"]
+      result = EventHandler.getScopes(frame_id)
+
+    elseif method == "variables"
+      var_ref = params["variablesReference"]
+      result = EventHandler.getVariables(var_ref)
+
     else
-      event_method = "stopped"
-      event = Dict("reason" => "entry",
-                   "description" => "stop on entry",
-                   "text" => " ")
+      # throw(MsgHandler.UnKnownMethod("$(method) can't be called"))
+      result = "$method can't be called"
     end
 
-  elseif method == "setBreakPoints"
-    filePath = params["path"]
-    lineno = params["lines"]
-    result = EventHandler.setBreakPoints(filePath, lineno)
+    # prepare respond
+    response = MsgHandler.msgCreate(id, result)
 
-  elseif method == "configurationDone"
-    result = Dict()
+    # send response
+    print(client, response)
 
-  elseif method == "continue"
-    result = EventHandler.continous()
-    event, event_method = EventHandler.getStatus("breakpoint")
-
-  elseif method == "next"
-    EventHandler.stepOver()
-    event, event_method = EventHandler.getStatus("step")
-
-  elseif method == "stackTrace"
-    result = EventHandler.getStackTrace()
-
-  elseif method == "scopes"
-    frame_id = params["frameId"]
-    result = EventHandler.getScopes(frame_id)
-
-  elseif method == "variables"
-    var_ref = params["variablesReference"]
-    result = EventHandler.getVariables(var_ref)
-
-  else
-    # throw(MsgHandler.UnKnownMethod("$(method) can't be called"))
-    result = "$method can't be called"
+    # prepare and send events (if have)
+    if event isa Dict
+      event = MsgHandler.eventCreate(event_method, event)
+      print(client, event)
+    end
   end
 
-  # prepare respond
-  response = MsgHandler.msgCreate(id, result)
+end 
 
-  # send response
-  print(client, response)
+end   # module debugger
 
-  # prepare and send events (if have)
-  if event isa Dict
-    event = MsgHandler.eventCreate(event_method, event)
-    print(client, event)
-  end
-
-end
+Connecter.run()

1206d39150b4c074d4e3904112e6a352b041679d
zhiqi lin <ralzq01@gmail.com>
2018-12-27 19:42:17 +0800

    update test files

--- test/breakpoint.jl
+++
@@ -1,4 +0,0 @@
-println("This is line 1")
-println("This is line 2")
-println("This is line 3")
-println("This is line 4")
--- test/main.jl
+++
@@ -1,9 +0,0 @@
-include("stacktrace.jl")
-a = 3
-
-b = 2
-
-function testMain()
-	testStackTrace()
-end
-
